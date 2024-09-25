0b0e33fb0d9b1991e26bb11460b5602e1da9e0ae
Diogo L Scarmagnani <diogols@live.com>
2024-09-25 20:28:09 -0300

    full

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// import Chart from 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js'
-// import Chart from 'chart.js/auto';
-// export { Chart }
 
 import { TabelaAbastecimentos } from './TabelaAbastecimentos.js'
 export const MAIN_ROOT = document.querySelector('#root')
